365986e20ebac39b28bac85581cfa600be4367b2
ZoziCoder5150 <saudzomo@gmail.com>
2024-02-20 17:02:57 +0300

    basic code for reading temp path

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
-/*
-    Hello, World!
-*/
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <cstdlib>
+
+int main(int argc, char * argv[]) {
+    std::cout << getenv("TMP");
+    return 0;
+}
